a02350d81289a51fc96c829b698f1bfa4683a76d
Ytalo-Alves <ytaloalves10@hotmail.com>
2024-11-05 22:10:04 -0300

    chore: add commit-msg

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,17 @@ import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-export default async function Home() {
-
-  const {userId} = await auth()
-  if(!userId) {
-    redirect('/')
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
   }
 
   return (
     <div className="h-full flex items-center justify-center">
-      <UserButton showName/>
+      <UserButton showName />
     </div>
   );
-}
+};
+
+export default Home;
